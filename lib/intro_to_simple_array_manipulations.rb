931df366bc96db4b0c3f5df9a423d6427b108b2e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-13 18:56:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,21 @@ def using_concat(arg1,arg2)
 end
 
 def using_insert(Array,new_element)
-  Array.insert(arg2)
+  Array.insert(3,new_element)
+end
+
+def using_uniq(Array)
+  Array.uniq
+end
+
+def using_flatten(Array)
+  Array.flatten
+end
+
+def using_delete(Array,String)
+  Array.delete(String)
+end
+
+def using_delete_at(Array,Integer)
+  Array.delete(Integer)
 end
